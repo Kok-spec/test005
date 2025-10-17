ee6c169edb12666b75605e67ffb634bbc421b8ab
Kok-spec <kokoa.kawaraya.yw@ps.hitachi-solutions.com>
2025-10-17 17:11:35 +0900

    008

--- src/components/tailwind.config.js
+++ src/components/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}","./src/components/tailwind.css"],
   theme: {
     extend: {
         fontFamily: {
